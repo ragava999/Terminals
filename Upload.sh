eaf809ed61fd534456b2532bf8dc9e3c6d916a36
Oliver Kohl D.Sc <oliver@kohl.bz>
2016-11-02 13:00:01 +0100

    Update Upload.sh

--- Upload.sh
+++ Upload.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+######## DOWNLOAD AND INVOKE "GHR" - a tool capable of uploading a release to GitHub
 
+echo Grabbing GHR
+
+#go get -u -f -v github.com/tcnksm/ghr
+
+wget https://github.com/tcnksm/ghr/releases/download/v0.5.3/ghr_v0.5.3_linux_amd64.zip
+
+uzip ghr_v0.5.3_linux_amd64.zip -d .
+
+echo Finished downloading GHR. Progressing with the upload.
+
+ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION $ARTIFACTS
+ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION "/home/ubuntu/Terminals/Terminals.Setup/Output/"
+
+echo Uploads have been completed successfully.
