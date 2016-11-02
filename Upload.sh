9f4e1a62e384d79a0834305550daf8a6c0ba2da6
Oliver Kohl D.Sc <oliver@kohl.bz>
2016-11-02 15:19:02 +0100

    Update Upload.sh

--- Upload.sh
+++ Upload.sh
@@ -7,16 +7,9 @@ ARTIFACTS="/home/ubuntu/Terminals/Terminals/bin/x86/Release/"
 echo Grabbing GHR
 
 #go get -u -f -v github.com/tcnksm/ghr
+#ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION $ARTIFACTS
+#ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION "/home/ubuntu/Terminals/Terminals.Setup/Output/"
 
-wget https://github.com/tcnksm/ghr/releases/download/v0.5.3/ghr_v0.5.3_linux_amd64.zip
+wget https://github.com/tcnksm/ghr/releases/download/v0.5.3/ghr_v0.5.3_linux_amd64.zip && unzip ghr_v0.5.3_linux_amd64.zip -d . && echo Finished downloading GHR. Progressing with the upload.
 
-unzip ghr_v0.5.3_linux_amd64.zip -d .
-
-echo Finished downloading GHR. Progressing with the upload.
-
-ls -altr
-
-./ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION $ARTIFACTS
-./ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION "/home/ubuntu/Terminals/Terminals.Setup/Output/"
-
-echo Uploads have been completed successfully.
+./ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION $ARTIFACTS && ./ghr -t $GITHUB_TOKEN -u $CIRCLE_PROJECT_USERNAME -r $CIRCLE_PROJECT_REPONAME --replace $VERSION "/home/ubuntu/Terminals/Terminals.Setup/Output/" && echo Uploads have been completed successfully.
